de22c9a9ee4e5460e8760884300b0027cbcacba1
Daniel DeCovnick <danhd123@mac.com>
2015-06-16 03:33:16 -0700

    Initial Commit

---
+++ JarParser/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  JarParser
+//
+//  Created by Daniel DeCovnick on 6/16/15.
+//  Copyright © 2015 Softyards. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
